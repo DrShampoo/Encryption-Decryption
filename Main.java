5791574146ace98f3d2121841136723574b32bd7
Alexander <marrruska0@gmail.com>
2020-05-07 01:31:08 +0300

    Stage 1/6: Encrypted!

---
+++ Main.java
@@ -0,0 +1,10 @@
+package encryptdecrypt;
+
+public class Main {
+    public static void main(String[] args) {
+        String mySimple = new String ("abcdefghijklmnopqrstuvwxyz");
+        String myRevert = new String ("zyxwvutsrqponmlkjihgfedcba");
+        //"we found a treasure!"
+        System.out.println("dv ulfmw z givzhfiv!");
+    }
+}
