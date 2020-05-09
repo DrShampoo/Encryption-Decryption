24d795d35a046a13ae43730ac0ec31e495595366
Alexander <marrruska0@gmail.com>
2020-05-10 01:25:59 +0300

    Stage 2/6: Knowledge is key

--- Main.java
+++ Main.java
@@ -1,10 +1,24 @@
 package encryptdecrypt;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        String mySimple = new String ("abcdefghijklmnopqrstuvwxyz");
-        String myRevert = new String ("zyxwvutsrqponmlkjihgfedcba");
-        //"we found a treasure!"
-        System.out.println("dv ulfmw z givzhfiv!");
+        Scanner scanner = new Scanner(System.in);
+        char[] chars = scanner.nextLine().toCharArray();
+        int key = scanner.nextInt();
+        String mySimple = "abcdefghijklmnopqrstuvwxyz";
+//        String myRevert = new String ("zyxwvutsrqponmlkjihgfedcba");
+        char[] abc = mySimple.toCharArray();
+
+        for (int i = 0; i < chars.length; i++) {
+            for (int j = 0; j < abc.length; j++) {
+                if (chars[i] == abc[j]) {
+                    chars[i] = abc[(j + key) % 26];
+                    break;
+                }
+            }
+            System.out.print(chars[i]);
+        }
     }
 }
